881443b9f3eb3139cfc60df748774d3939bb93ba
Andrew Roth <andrewroth1991@gmail.com>
2014-07-18 14:39:25 -0500

    Obtaining images implemented
    RikkaBot can now authenticate as an Imgur user and download a random
    image from a provided album.

--- main.py
+++ main.py
@@ -1,27 +1,72 @@
 import argparse
 import json
 import os
+import random
+
+from oauth.oauth2 import Consumer
 
 DATA_FILE = 'data.json'
+IMGUR_AUTH_URL = 'https://api.imgur.com/oauth2/authorize'
+IMGUR_TOKEN_URL = 'https://api.imgur.com/oauth2/token'
 
-def main(client_id):
-    if not os.path.exists(DATA_FILE):
+def main(client_id, client_secret, album_name):
+    refresh_token = None
+    consumer = Consumer(client_id, client_secret)
+
+
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            data = json.loads(f.read())
+        try:
+            refresh_token = data['refresh_token']
+        except KeyError:
+            print 'Keyfile invalid, need to re-authorize.'
+
+    if not refresh_token:
+        refresh_token = consumer.authorize(
+            IMGUR_AUTH_URL, IMGUR_TOKEN_URL, response_type='pin'
+        )
         with open(DATA_FILE, 'w') as f:
-            f.write(json.dumps({'refresh_token': None}))
+            data = json.dumps({'refresh_token': refresh_token})
+            f.write(data)
 
-    with open(DATA_FILE, 'r') as f:
-        data = json.loads(f.read())
-    refresh_token = data['refresh_token']
+    access_token = consumer.get_access_token(IMGUR_TOKEN_URL, refresh_token)
+    headers = {'Authorization': 'Bearer {0}'.format(access_token)}
+    albums = consumer.api_request(
+        'https://api.imgur.com/3/account/me/albums/', headers
+    
+    )
+    album_id = None
+    for album in albums:
+        if album['title'] == album_name:
+            album_id = album['id']
+    data = consumer.api_request(
+        'https://api.imgur.com/3/account/me/album/{1}'.format(album_id), 
+        headers
+    )
+    images = [image['link'] for image in data['images']]
+
+    print random.sample(images, 1)[0]
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--client-id', action='store', type=str, default=None,
-        help='The client id needed to register an application'
+        help='The client id needed to authorize an application'
+    )
+
+    parser.add_argument(
+        '--client-secret', action='store', type=str, default=None, 
+        help='The client secret needed to authorize an application'
+    )
+
+    parser.add_argument(
+        '--album', action='store', type=str, default=None,
+        help='The imgur album to grab images from'
     )
 
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.client_id)
+    main(args.client_id, args.client_secret, args.album)
--- oauth/oauth2.py
+++ oauth/oauth2.py
@@ -1,24 +1,40 @@
 import json
 import requests
-import webbrowser
 
-AUTH_URL = 'https://api.imgur.com/oauth2/authorize'
-TOKEN_URL = 'https://api.imgur.com/oauth2/token'
-RESPONSE_TYPE = 'pin'
+class Consumer:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-def authorize(client_id, client_secret):
-    url = '{0}?client_id={1}&response_type={2}'.format(
-        AUTH_URL, client_id, RESPONSE_TYPE
-    )
+    def authorize(self, auth_url, token_url, **kwargs):
+        url = '{0}?client_id={1}'.format(auth_url, self.client_id)
+        if kwargs:
+            url += '&{0}'.format('&'.join(
+                ['{0}={1}'.format(key, val) for key, val in kwargs.items()]
+            ))
 
-    print 'Go to {0} in a webrowser!'.format(url)
-    pin = raw_input('Please enter the PIN provided by the website: ')
+        print 'Go to {0} in a webrowser!'.format(url)
+        pin = raw_input('Please enter the PIN provided by the website: ')
 
-    payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'pin',
-        'pin': pin
-    }
-    r = requests.post(TOKEN_URL, data=json.dumps(payload))
-    refresh_token = json.loads(r.text)['refresh_token']
+        payload = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'pin',
+            'pin': pin
+        }
+        r = requests.post(token_url, data=json.dumps(payload))
+        return json.loads(r.text)['refresh_token']
+
+    def get_access_token(self, token_url, refresh_token):
+        payload = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'refresh_token': refresh_token,
+            'grant_type': 'refresh_token'
+        }
+        r = requests.post(token_url, data=json.dumps(payload))
+        return json.loads(r.text)['access_token']
+
+    def api_request(self, url, headers):
+        r = requests.get(url, headers=headers)
+        return json.loads(r.text)['data']
